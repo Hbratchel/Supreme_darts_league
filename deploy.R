e4731b7ab2287041e6e012ff07e58321a75bfb8c
Hbratchel <71376572+Hbratchel@users.noreply.github.com>
2023-01-25 20:33:52 +0000

    basic script to deploy updated app to shinyapps.io

---
+++ deploy.R
@@ -0,0 +1,8 @@
+###################################
+#v basic Script to deploy app to shiny.io - needs revision
+
+## if not installed
+#install.packages('rsconnect')
+library(rsconnect)
+
+deployApp()
